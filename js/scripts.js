048a7cecba2b7e5bdbf30ba485111527631dbfed
Robin Keybs <Robin@Robins-MacBook.local>
2019-03-29 11:57:03 +0300

    test js business and logic function ,without array

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//===business logic===//
+
+$(document).ready(function(){
+    $("#blankForm").submit(function(event){
+        var quiz1 = $("#quiz1").val();
+        var quiz2 = $("#quiz2").val();
+
+    var resultScore = parseInt(quiz1)=parseInt(quiz2);
+    $("#Finish").text("You Scored: "+ resultScore + "out of 100");
+
+
+    //=========user Interface=======//
+    $("Finish").show();
+    event.preventDefault();
+});
+});
