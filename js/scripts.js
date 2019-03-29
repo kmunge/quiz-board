818d0f029cd947845b008e907256c086ba0a3c32
Robin Keybs <Robin@Robins-MacBook.local>
2019-03-29 16:19:55 +0300

    adding value questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-//===business logic===//
+//==== Business logic====//
 
-$(document).ready(function(){
-    $("#blankForm").submit(function(event){
-        var quiz1 = $("#quiz1").val();
-        var quiz2 = $("#quiz2").val();
+$(document).ready(function() {
+$("#blankForm").submit(function(event){
+    var quiz1 = $("#quiz1").val();
+    var quiz2 = $("#quiz2").val();
+    var quiz3 = $("#quiz3").val();
+    var quiz4 = $("#quiz4").val();
 
-    var resultScore = parseInt(quiz1)=parseInt(quiz2);
-    $("#Finish").text("You Scored: "+ resultScore + "out of 100");
+    var resultScore = parseInt(quiz1)+parseInt(quiz2)+parseInt(quiz3)+parseInt(quiz4);
+    $("#finish").text("You SCored: " + resultScore + " out of 100");
 
 
-    //=========user Interface=======//
-    $("finish").show();
+//==== User Interface ====//
+    $("#finish").show();
     event.preventDefault();
 });
+
 });
+
